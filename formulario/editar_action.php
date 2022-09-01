cb99356df031ac65a941ce3d3daa66c8fc7c42ec
Guilherme-Benso <guilhermesouzabenso@gmail.com>
2022-09-01 17:43:32 -0300

    new: new rows

---
+++ formulario/editar_action.php
@@ -0,0 +1,22 @@
+<?php
+require "conector.php";
+
+//declarando a variavel que vai armazenar o dado para mandar para o banco
+$id = filter_input(INPUT_POST, 'id');
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+//se estiver tudo certo continuar
+if ($nome && $email && $id) {
+    $sql = $pdo->prepare("UPDATE clientes SET nome = :name, email = :email where id = :id");
+    $sql->bindValue(":name", $name);
+    $sql->bindValue(":email", $email);
+    $sql->bindValue(":id",$id);
+    $sql->execute();
+    header("Location: index.php");
+    exit;
+}
+else {
+    header('Location:adicionar.php');
+    exit;
+}
