b19bb95fd4bbd93c8ebd404149de4f3513254c25
Guilherme-Benso <guilhermesouzabenso@gmail.com>
2022-09-02 12:16:28 -0300

    new & add: new folder and add rows

--- formulario/index.php
+++ formulario/index.php
@@ -1,12 +1,9 @@
 <?php
 require "conector.php";
-$lista = [];
+require "dao/UsuarioDaoMySQL.php";
+$usuarioDao = new UsuarioDaoMySQL($pdo); 
+$lista = $usuarioDao->findAll();
 
-$sql = $pdo->query("SELECT * FROM clientes");
-if ($sql->rowCount() > 0) {
-    //transformando os dados em um array, o FETCH_ASSOC associa os campos
-    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
-}
 ?>
 <a href="adicionar.php">ADICIONAR USUÀRIO</a>
 <table border="1" width="100%">
@@ -19,9 +16,9 @@ if ($sql->rowCount() > 0) {
     <?php foreach ($lista as $usuario):  ?> 
         <!--//mostrando os dados  -->
         <tr>    
-            <td><?=$usuario['id'];?></td>
-            <td><?=$usuario['nome'];?></td>
-            <td><?=$usuario['email'];?></td>
+            <td><?=$usuario->getId();?></td>
+            <td><?=$usuario->getNome();?></td>
+            <td><?=$usuario->getEmail();?></td>
             <td>
                 <a href="editar.php?id=<?=$usuario["id"];?>">[ Editar ]</a>
                 <a href="deletar_action.php?id=<?=$usuario["id"];?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
